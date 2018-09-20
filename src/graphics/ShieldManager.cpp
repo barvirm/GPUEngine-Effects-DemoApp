9e7536318d8c1a475d36d5b9345f0933c1d8804f
Marek <barvirm@seznam.cz>
2018-09-20 15:34:23 +0200

    Prepare animation for intersection and pulsewave on shield

--- src/graphics/ShieldManager.cpp
+++ src/graphics/ShieldManager.cpp
@@ -1,9 +1,17 @@
 #include <ShieldManager.h>
 
 #include <Effects/shield/Shield.h>
+#include <Effects/shield/Intersection.h>
+#include <Effects/shield/PulseWave.h>
+#include <geSG/Animation.h>
+#include <sg/AnimationChannel.h>
+#include <sg/AnimationManager.h>
 #include <iostream>
 #include <glm/vec3.hpp>
 #include <glm/mat4x4.hpp>
+#include <memory>
+
+
 
 void msg::ShieldManager::addShield(glm::vec3 center, float radius) {
     auto m(std::make_shared<glm::mat4>(
@@ -19,5 +27,40 @@ void msg::ShieldManager::addShield(glm::vec3 center, float radius) {
 msg::ShieldManager::ShieldManager() {
     std::cout << "ShieldManager ctor" << std::endl;
     shields = std::make_shared<std::vector<msg::Shield>>();
+}
+
+void msg::ShieldManager::addPulseWave(msg::Shield &shield, glm::vec3 origin) {
+    msg::PulseWave pulseWave(0, origin);
+
+    auto anim(std::make_shared<ge::sg::Animation>());
+    auto floatChannel(std::make_shared<msg::AnimationChannel<float>>());
+    auto PulseWavetime = pulseWave.getTime();
+
+    floatChannel->setTarget(PulseWavetime);
+    anim->channels.push_back(floatChannel);
+
+    floatChannel->KF.emplace_back(std::chrono::seconds(0), 0);
+    floatChannel->KF.emplace_back(std::chrono::seconds(3), 1);
+    shield.addPulseWave(pulseWave);
+
+    animationManager->playAnimation(anim, ge::core::time_point(std::chrono::duration<double>(*time.get())));
+
+}
+
+void msg::ShieldManager::addIntersection(msg::Shield &shield, glm::vec3 origin, ge::core::time_point &t) {
+    msg::Intersection intersection(origin);
+
+    auto anim(std::make_shared<ge::sg::Animation>());
+    auto floatChannel(std::make_shared<msg::AnimationChannel<float>>());
+    auto target = intersection.time();
+    floatChannel->setTarget(target);
+    anim->channels.push_back(floatChannel);
+
+    floatChannel->KF.emplace_back(std::chrono::seconds(0), 0.0f);
+    floatChannel->KF.emplace_back(t, 1.0f);
+
+    shield.addIntersection(intersection);
+    animationManager->playAnimation(anim, ge::core::time_point(std::chrono::duration<double>(*time.get())));
+
 }
 
--- src/graphics/ShieldManager.h
+++ src/graphics/ShieldManager.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <glm/fwd.hpp>
 #include <vector>
+#include <geCore/Updatable.h>
 
 
 namespace msg {
@@ -17,6 +18,8 @@ namespace msg {
             ~ShieldManager() = default;
 
             void addShield(glm::vec3 center, float radius);
+            void addPulseWave(msg::Shield &shield, glm::vec3 origin);
+            void addIntersection(msg::Shield &shield, glm::vec3 origin, ge::core::time_point &t);
             
             std::shared_ptr<std::vector<msg::Shield>> shields;
             std::shared_ptr<msg::AnimationManager> animationManager;
